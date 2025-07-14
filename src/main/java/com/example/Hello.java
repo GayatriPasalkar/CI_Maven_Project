412933f33663941fa3cf70eaf63b33723c0f9027
Gayatri <gayatripasalkar03@gmail.com>
2025-07-14 21:24:11 +0530

    Test build trigger via Poll SCM

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from CI with Jenkins and Maven!");
+	System.out.println("Hello from Jenkins Poll SCM Test!");
     }
 }
